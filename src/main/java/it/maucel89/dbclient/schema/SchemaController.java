fda046f990ef31965d646fa63b41324e9eaa75ed
Mauro Celani <mauro.celani@smc.it>
2018-09-29 13:14:57 +0200

    Now we can populate table with data

--- src/main/java/it/maucel89/dbclient/schema/SchemaController.java
+++ src/main/java/it/maucel89/dbclient/schema/SchemaController.java
@@ -2,23 +2,26 @@ package it.maucel89.dbclient.schema;
 
 import it.maucel89.dbclient.DbConnection;
 import it.maucel89.dbclient.util.AbsController;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.util.Callback;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * @author Mauro Celani
@@ -37,6 +40,9 @@ public class SchemaController extends AbsController {
 
 	@FXML
 	private TableView<Column> columnsTableView;
+
+	@FXML
+	private TableView dataTableView;
 
 	public void initData(DbConnection dbConnection) throws SQLException {
 
@@ -84,30 +90,109 @@ public class SchemaController extends AbsController {
 			(observable, oldValue, newValue) -> {
 //				System.out.print(newValue);
 
-				columnsTableView.getItems().removeAll(
-					columnsTableView.getItems());
+				String schema = dbConnection.getSchema();
+				String table = newValue;
 
-				try {
-					ResultSet rs = dbmd.getColumns(
-						dbConnection.getSchema(), null, newValue, "%");
-
-					while (rs.next()) {
-//						tablesViewData.add(rs.getString("TABLE_NAME"));
-//						System.out.println(rs.getString(4));
-//						System.out.println(rs.getString("COLUMN_NAME"));
-						columnsTableView.getItems().add(Column.fromRow(rs));
-					}
-				}
-				catch (SQLException ex) {
-					// handle any errors
-					System.out.println("SQLException: " + ex.getMessage());
-					System.out.println("SQLState: " + ex.getSQLState());
-					System.out.println("VendorError: " + ex.getErrorCode());
-
-					showAlert(AlertType.ERROR, "Lettrua colonne non riuscita!");
-				}
+				populateSchemaColumns(dbmd, schema, table);
+				populateTableData(conn, schema, table);
 			}
 		);
+	}
+
+	private void populateSchemaColumns(
+		DatabaseMetaData dbmd, String schema, String table) {
+
+		// EMPTY OLD DATA
+		columnsTableView.getItems().clear();
+
+		try {
+			ResultSet rs = dbmd.getColumns(
+				schema, null, table, "%");
+
+			while (rs.next()) {
+				columnsTableView.getItems().add(Column.fromRow(rs));
+			}
+		}
+		catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+
+			showAlert(AlertType.ERROR, "Lettrua colonne non riuscita!");
+		}
+	}
+
+	private void populateTableData(
+		Connection conn, String schema, String table) {
+
+		// EMPTY OLD DATA
+		dataTableView.getItems().clear();
+		dataTableView.getColumns().clear();
+
+		ObservableList<Column> columns = columnsTableView.getItems();
+
+		try {
+
+			String sql = "SELECT ";
+
+			for (int i = 0; i < columns.size(); i++) {
+
+				String name = columns.get(i).getName();
+
+				sql += name + ", ";
+
+
+			}
+			sql = sql.substring(0, sql.length() - 2);
+
+			sql += " FROM " + table;
+
+			PreparedStatement statement = conn.prepareStatement(sql);
+
+			ResultSet rs = statement.executeQuery();
+
+
+			for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+				final int j = i;
+
+				TableColumn tableColumn = new TableColumn(
+					rs.getMetaData().getColumnName(i + 1));
+				tableColumn.setCellValueFactory(
+					(Callback<CellDataFeatures<ObservableList, String>,
+						ObservableValue<String>>) param -> {
+
+						String value = "";
+						if (param.getValue().get(j) != null) {
+							value = param.getValue().get(j).toString();
+						}
+						return new SimpleStringProperty(value);
+					}
+
+				);
+
+				dataTableView.getColumns().addAll(tableColumn);
+			}
+
+			while (rs.next()) {
+//				dataTableView.getItems().add(rs);
+				ObservableList<String> row = FXCollections.observableArrayList();
+				for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+					row.add(rs.getString(i));
+				}
+				//System.out.println(row);
+				dataTableView.getItems().add(row);
+			}
+
+		}
+		catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+
+			showAlert(AlertType.ERROR, "Lettrua dati non riuscita!");
+		}
 	}
 
 
